da40083430e649f41834b2e53c292210c4e23051
Muhammad Arslan <58504655+arslanbisharat@users.noreply.github.com>
2020-04-07 00:34:29 +0500

    Update enumerable_project.rb

--- enumerable_project.rb
+++ enumerable_project.rb
@@ -1,94 +1,91 @@
-# rubocop:disable Metrics/CyclomaticComplexity
-# rubocop:disable Metrics/PerceivedComplexity
-# rubocop:disable Metrics/ModuleLength
+# frozen_string_literal: true
 
 module Enumerable
   def my_each
-    self.length.times do |e|
-      yield(self[e])
+    i = 0
+    while self[i]
+      yield(self[i])
+      i += 1
     end
   end
-  
+
   def my_each_with_index
-    self.length.times do |e|
-      yield(self[e], e)
-    end
+    my_each { |e| yield(e, index(e)) }
   end
-  
+
   def my_select
-    self.length.times do |e|
-      if yield(self[e])
-        puts self[e]
-      end
+    result = []
+    each do |e|
+      result << e if yield(e)
     end
+    result
   end
-  
+
   def my_all?
-    self.length.times do |e|
-      if yield(self[e]) == false
-        return false
-      end
+    my_each do |e|
+      return false unless yield(e)
     end
-    return true
+    true
   end
-  
+
   def my_any?
-    self.length.times do |e|
-      if yield(self[e])
-        return true
-      end
+    my_each do |e|
+      return true if yield(e)
     end
-    return false
+    false
   end
-  
+
   def my_none?
-    self.length.times do |e|
-      if yield(self[e])
-        return false
-      end
+    return true unless block_given?
+
+    my_each do |e|
+      return true unless yield(e)
     end
-    return true
+    false
   end
-  
-  def my_count
-    counter = 0
-    self.length.times do |e|
-      if yield(self[e])
-        counter += 1
-      end
+
+  def my_count(obj = nil)
+    count = 0
+    my_each do |e|
+      count += 1
+      return count if obj && count == obj
+      return count if block_given? && yield(e)
     end
-    return counter
+    return count unless block_given?
   end
-  
-  def my_map(proc=nil)
-    newArr = []
-      if proc
-        self.length.times do |e|
-        newArr[e] = proc.call(self[e])
-         end
-      else
-        self.length.times do |e|
-        newArr[e] = yield(self[e])
-         end
-        end
-    puts newArr
-  end
-  
-  def my_inject(go = 0)
-    acum = go
-    self.length.times do |e|
-      acum = yield(acum, self[e])
+
+  def my_map(&block)
+    arr = []
+    my_each do |e|
+      arr << block.call(e)
     end
-    return acum
+    arr
   end
-  
-  def multiply_els
-    self.my_inject(1) do |k, e|
-      k *= e
+
+  def my_inject
+    memo = self[0]
+    my_each do |e|
+      memo = yield(memo, e)
     end
+    memo
   end
 end
-  
-# rubocop:enable Metrics/CyclomaticComplexity
-# rubocop:enable Metrics/PerceivedComplexity
-# rubocop:enable Metrics/ModuleLength
+
+# arr = [4, 'hey', 2, 'Hi there!']
+arr = [1, 2, 3, 4]
+
+# p arr.my_each
+# arr.my_each_with_index { |e, i| p "#{e}, #{i}" }
+# p arr.my_select { |e| e.is_a? String }
+# p arr.my_all? { |e| e.is_a? String }
+# p arr.my_any? { |e| e.is_a? String }
+# p arr.my_none? { |e| e == 99 }
+# p arr.my_count
+
+double_els = ->(e) { e * 2 }
+p arr.my_map { |e| double_els.call(e) }
+
+def multiply_els(arr)
+  arr.my_inject { |memo, e| memo * e }
+end
+p multiply_els([2, 4, 5])
