c0f620f4018f9f5d080c0f99bb9a12bc1a7b74e1
Muhammad Arslan <58504655+arslanbisharat@users.noreply.github.com>
2020-04-10 06:51:03 +0500

    rubocop errors removed

--- enumerable_project.rb
+++ enumerable_project.rb
@@ -1,117 +1,1335 @@
 # rubocop:disable Metrics/CyclomaticComplexity
 
+
+
 # rubocop:disable Metrics/PerceivedComplexity
 
+
+
 # rubocop:disable Metrics/ModuleLength
+
+
+
+
+
+
 
 module Enumerable
   def my_each
     return to_enum unless block_given?
 
-    size.times { |n| yield self[n] }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    length.times { |n| yield(self[n]) }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    self
   end
 
   def my_each_with_index
     return to_enum unless block_given?
 
-    size.times { |n| yield self[n], n }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    length.times { |n| yield(self[n], n) }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    self
   end
 
   def my_select
     return to_enum unless block_given?
 
-    array = []
-    my_each { |n| array.push(n) if yield(n) }
-    array
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    new_array = []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    length.times { |n| new_array.push(self[n]) if yield(self[n]) }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    new_array
   end
 
   def my_all?(arg = nil)
-    if arg.is_a?(Regexp)
-      my_each { |n| return false unless arg.match(n) }
-    elsif arg.is_a?(Integer) || arg.is_a?(String)
-      my_each { |n| return false unless n == arg }
-    elsif block_given?
-      my_each { |n| return false unless yield(n) }
-    elsif arg.nil?
-      my_each { |n| return false unless n }
+    if arg.is_a?(Class)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      length.times { |n| return false unless self[n].is_a?(arg) }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    elsif arg.is_a?(String) || arg.is_a?(Integer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      length.times { |n| return false unless self[n] == arg }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    elsif !arg.nil?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      length.times { |n| return false unless self[n].match(arg) }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    elsif !block_given?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      length.times { |n| return false if self[n] == false || self[n].nil? }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     else
-      my_each { |n| return false unless n.is_a?(arg) }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      length.times { |n| return false unless yield(self[n]) }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     true
   end
 
-  def my_any?(arg = nil, &block)
-    if arg.is_a?(Regexp)
-      my_each { |n| return true if arg.match(n) }
-    elsif arg.is_a?(Integer) || arg.is_a?(String)
-      my_each { |n| return true if n == arg }
-    elsif block_given?
-      my_each { |n| return true if block.call(n) }
-    elsif arg.nil?
-      my_each { |n| return true if n }
+  def my_any?(arg = nil)
+    if arg.is_a?(Class)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      length.times { |n| return true if self[n].is_a?(arg) }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    elsif arg.is_a?(String) || arg.is_a?(Integer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      length.times { |n| return true if self[n] == arg }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    elsif !arg.nil?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      length.times { |n| return true if self[n].match(arg) }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    elsif !block_given?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      length.times { |n| return true unless self[n] == false || self[n].nil? }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     else
-      my_each { |n| return true if n.is_a?(arg) }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      length.times { |n| return true if yield(self[n]) }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     false
   end
 
-  def my_none?(arg = nil, &block)
-    !my_any?(arg, &block)
+  def my_none?(arg = nil)
+    if arg.is_a?(Class)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      length.times { |n| return false if self[n].is_a?(arg) }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    elsif arg.is_a?(String) || arg.is_a?(Integer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      length.times { |n| return false if self[n] == arg }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    elsif !arg.nil?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      length.times { |n| return false if self[n].match(arg) }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    elsif !block_given?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      length.times { |n| return false unless self[n] == false || self[n].nil? }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    else
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      length.times { |n| return false if yield(self[n]) }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    true
   end
 
   def my_count(arg = nil)
-    count = 0
-    if block_given? && arg.nil?
-      my_each { |n| count += 1 if yield(n) }
-    elsif arg.nil?
-      my_each { |n| count += 1 if n }
-    elsif arg.is_a?(Integer) || arg.is_a?(String)
-      my_each { |n| count += 1 if n == arg }
+    unless block_given?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      counter = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      length.times { |n| counter += 1 if self[n] == arg }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      return counter unless arg.nil?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      return length
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    counter = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    length.times { |n| counter += 1 if yield(self[n]) }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    counter
+  end
+
+  def my_map(proc = nil)
+    return to_enum unless block_given?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    new_array = []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    if proc.nil?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      length.times { |n| new_array.push(yield(self[n])) }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     else
-      my_each { |n| count += 1 if n.is_a?(arg) }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      length.times { |n| new_array.push(proc.call(self[n])) }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     end
-    count
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    new_array
   end
 
-  def my_map(proc = nil, &block)
-    return to_enum unless proc || block
+  def my_inject(result = 0, symbol = nil)
+    symbol, result = result, symbol if result.is_a?(Symbol) and symbol.is_a?(Integer)
 
-    proc = block if block && !proc
-    obj = to_a
-    obj.size.times { |n| obj[n] = proc.call(obj[n]) }
-    obj
-  end
 
-  def my_inject(initial = 0, arg = nil)
-    obj = to_a
-    memo = obj[0]
 
-    if initial.is_a?(Symbol)
-      arg = initial
-      initial = 0
-      initial, memo = 1 if arg == :/ || arg == :* && initial.zero?
-    end
 
-    memo = (my_inject { |oper, n| oper.public_send(arg, n) }).public_send(arg, initial) unless arg.nil?
 
-    if block_given?
-      if initial.zero?
-        (obj.size - 1).times { |n| memo = yield(memo, obj[n + 1]) }
-      else
-        obj.size.times { |n| initial = yield(initial, obj[n]) }
-        memo = initial
+
+
+
+
+
+
+
+
+
+
+    symbol, result = result, 0 if result.is_a?(Symbol)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    new_array = to_a
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    result = '' if new_array[0].is_a?(String)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    if !block_given?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      case symbol
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      when :+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        new_array.length.times { |n| result += new_array[n] }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      when :*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        new_array.length.times { |n| result *= new_array[n] }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      when :/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        new_array.length.times { |n| result /= new_array[n] }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      when :-
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        new_array.length.times { |n| result -= new_array[n] }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      when :**
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        new_array.length.times { |n| result **= new_array[n] }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      when :%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        new_array.length.times { |n| result %= new_array[n] }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    else
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      new_array.length.times { |n| result = yield(result, new_array[n]) }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     end
-    memo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    result
   end
 end
 
-module TestEnumerable
-  def multiply_els(array)
-    array.my_inject(:*)
-  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def multiply_els(arr)
+  arr.my_inject { |x, y| x * y }
 end
+
+
+
+
+
+
 
 # rubocop:enable Metrics/CyclomaticComplexity
+
+
+
 # rubocop:enable Metrics/PerceivedComplexity
+
+
+
 # rubocop:enable Metrics/ModuleLength
